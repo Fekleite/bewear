d892355769113bac17aa8830407cb1818627ca2d
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-08-21 22:47:48 -0300

    feat: create header component

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import { SignInForm } from "./sign-in-form";
@@ -5,21 +6,25 @@ import { SignUpForm } from "./sign-up-form";
 
 export default function AuthPage() {
   return (
-    <div className="flex h-screen w-full max-w-sm flex-col justify-center gap-6 px-3.5">
-      <Tabs defaultValue="sign-in">
-        <TabsList>
-          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-          <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
-        </TabsList>
+    <div className="flex h-screen flex-col">
+      <Header />
 
-        <TabsContent value="sign-in">
-          <SignInForm />
-        </TabsContent>
+      <div className="mx-auto flex w-full max-w-sm flex-1 flex-col gap-6 px-3.5 md:max-w-md lg:max-w-lg lg:justify-center">
+        <Tabs defaultValue="sign-in">
+          <TabsList>
+            <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+            <TabsTrigger value="sign-up">Criar conta</TabsTrigger>
+          </TabsList>
 
-        <TabsContent value="sign-up">
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
+          <TabsContent value="sign-in">
+            <SignInForm />
+          </TabsContent>
+
+          <TabsContent value="sign-up">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }
---
+++ src/components/common/header.tsx
@@ -0,0 +1,29 @@
+import { ShoppingBagIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+import { Menu } from "./menu";
+
+export function Header() {
+  return (
+    <header className="flex h-[72px] w-full items-center justify-between px-6 py-4">
+      <Link href="/">
+        <Image src="/bewear-logo.svg" alt="BEWEAR" width={92} height={24} />
+      </Link>
+
+      <div className="flex h-full items-center gap-4 lg:hidden">
+        <Button variant="ghost" className="text-muted-foreground">
+          <ShoppingBagIcon />
+
+          <span className="sr-only">Sacola de compras</span>
+        </Button>
+
+        <Separator orientation="vertical" className="max-h-4" />
+
+        <Menu />
+      </div>
+    </header>
+  );
+}
