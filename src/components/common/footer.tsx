6858aaf615e78d1945fff72484b618e28d833114
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-09-02 20:06:15 -0300

    feat: adjust footer to be responsive

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,13 +1,16 @@
 export function Footer() {
   const currentYear = new Date().getFullYear();
+
   return (
-    <footer className="bg-muted p-8">
-      <span className="text-xs font-medium">
-        &copy; {currentYear} Copyright BEWEAR
-      </span>
-      <span className="text-muted-foreground block text-xs font-medium">
-        Todos os direitos reservados.
-      </span>
+    <footer className="bg-muted w-full">
+      <div className="p-8 lg:mx-auto lg:max-w-[1440px] lg:px-11">
+        <span className="text-xs font-medium">
+          &copy; {currentYear} Copyright BEWEAR
+        </span>
+        <span className="text-muted-foreground block text-xs font-medium">
+          Todos os direitos reservados.
+        </span>
+      </div>
     </footer>
   );
 }
