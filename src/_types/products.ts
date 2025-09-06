696c558af989869821953582609a13a694230e8d
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-09-06 15:27:09 -0300

    chore: update exports from product types

--- src/_types/products.ts
+++ src/_types/products.ts
@@ -1,7 +1,7 @@
 import { productTable, productVariantTable } from "@/db/schema";
 
-type ProductType = typeof productTable.$inferSelect;
-type VariantType = typeof productVariantTable.$inferSelect;
+export type ProductType = typeof productTable.$inferSelect;
+export type VariantType = typeof productVariantTable.$inferSelect;
 
 export interface Product extends ProductType {
   variants: VariantType[];
