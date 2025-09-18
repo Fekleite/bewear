f3f46670bb34e2e1c58295a7c4f3c4bb9ad4d978
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-09-17 22:13:29 -0300

    feat: create checkout identification page

---
+++ src/app/(home)/checkout/(form)/identification/page.tsx
@@ -0,0 +1,50 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { getCart } from "@/actions/get-cart";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { db } from "@/db";
+import { shippingAddressTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+import { ShippingForm } from "./shipping-form";
+
+export default async function Identification() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user.id) {
+    redirect("/");
+  }
+
+  const cart = await getCart();
+
+  if (!cart || cart?.items.length === 0) {
+    redirect("/");
+  }
+
+  const shippingAddresses = await db.query.shippingAddressTable.findMany({
+    where: eq(shippingAddressTable.userId, session.user.id),
+  });
+
+  return (
+    <>
+      <Card className="w-full lg:max-w-xl">
+        <CardHeader>
+          <CardTitle className="font-semibold lg:text-2xl">
+            Identificação
+          </CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <ShippingForm
+            shippingAddresses={shippingAddresses}
+            defaultShippingAddressId={cart.shippingAddressId}
+          />
+        </CardContent>
+      </Card>
+    </>
+  );
+}
