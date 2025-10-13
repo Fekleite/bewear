16dd9efc5bf0ea277214fa540a74e7bb968c2610
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-10-13 20:38:18 -0300

    feat: create order placed modal

--- src/app/(home)/checkout/(form)/payment/complete-order-button.tsx
+++ src/app/(home)/checkout/(form)/payment/complete-order-button.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { Loader2Icon } from "lucide-react";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { useCompleteOrder } from "@/hooks/mutations/use-complete-order";
 
+import { OrderPlacedModal } from "./order-placed-modal";
+
 export function CompleteOrderButton() {
+  const [isModalOpen, setIsModalOpen] = useState(true);
   const { mutate, isPending } = useCompleteOrder();
 
   function handleCompleteOrder() {
@@ -13,14 +17,18 @@ export function CompleteOrderButton() {
   }
 
   return (
-    <Button
-      size="lg"
-      className="w-full rounded-full"
-      onClick={handleCompleteOrder}
-      disabled={isPending}
-    >
-      {isPending && <Loader2Icon className="animate-spin" />}
-      Finalizar a compra
-    </Button>
+    <>
+      <Button
+        size="lg"
+        className="w-full rounded-full"
+        onClick={handleCompleteOrder}
+        disabled={isPending}
+      >
+        {isPending && <Loader2Icon className="animate-spin" />}
+        Finalizar a compra
+      </Button>
+
+      <OrderPlacedModal isOpen={isModalOpen} onOpenChange={setIsModalOpen} />
+    </>
   );
 }
---
+++ src/app/(home)/checkout/(form)/payment/order-placed-modal.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+interface OrderPlacedModalProps {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export function OrderPlacedModal({
+  isOpen,
+  onOpenChange,
+}: OrderPlacedModalProps) {
+  return (
+    <Dialog open={isOpen} onOpenChange={onOpenChange}>
+      <DialogContent className="flex flex-col items-center gap-8 py-8">
+        <Image
+          src="/illustration.svg"
+          alt="Order placed illustration"
+          width={250}
+          height={234}
+          className="mx-auto"
+        />
+
+        <div className="space-y-6 text-center">
+          <DialogTitle className="text-2xl font-semibold">
+            Pedido Efetuado!
+          </DialogTitle>
+
+          <DialogDescription className="text-muted-foreground text-sm font-medium">
+            Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
+            na seção de “Meus Pedidos”.
+          </DialogDescription>
+        </div>
+
+        <div className="space-y-3">
+          <Button size="lg" className="w-full rounded-full">
+            Ver meu pedido
+          </Button>
+
+          <Button
+            variant="link"
+            className="text-foreground w-full rounded-full"
+          >
+            Página inicial
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
