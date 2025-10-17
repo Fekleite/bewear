277ddce4b563b228e06ef85eb11227b5703b1718
Fernanda Leite <dev.fernandaleite@gmail.com>
2025-10-16 21:23:40 -0300

    feat: redirect to stripe checkout

--- src/app/(home)/checkout/(form)/payment/complete-order-button.tsx
+++ src/app/(home)/checkout/(form)/payment/complete-order-button.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { loadStripe } from "@stripe/stripe-js";
 import { Loader2Icon } from "lucide-react";
 import { useState } from "react";
 
+import { createCheckoutSession } from "@/actions/create-checkout-session";
 import { Button } from "@/components/ui/button";
 import { useCompleteOrder } from "@/hooks/mutations/use-complete-order";
 
@@ -10,11 +12,30 @@ import { OrderPlacedModal } from "./order-placed-modal";
 
 export function CompleteOrderButton() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { mutate, isPending } = useCompleteOrder();
+  const { mutateAsync, isPending } = useCompleteOrder();
 
-  function handleCompleteOrder() {
-    mutate();
-    setIsModalOpen(true);
+  async function handleCompleteOrder() {
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Stripe publishable key is not set");
+    }
+
+    const { orderId } = await mutateAsync();
+
+    const checkoutSession = await createCheckoutSession({
+      orderId,
+    });
+
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+
+    if (!stripe) {
+      throw new Error("Failed to load Stripe");
+    }
+
+    await stripe.redirectToCheckout({
+      sessionId: checkoutSession.id,
+    });
   }
 
   return (
